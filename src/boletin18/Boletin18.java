cb043b8186c7c84eeb8d4032c4b65d799c7f6b55
Daniel Martinez <dani@danielcastelao.org>
2018-02-01 14:14:03 +0100

    finalizado v1 mod1

--- src/boletin18/Boletin18.java
+++ src/boletin18/Boletin18.java
@@ -20,42 +20,42 @@ public class Boletin18{
         Buzon buzon1=new Buzon();
         int opcion=0;
         boolean encendido=true;
-        
-    while(encendido==true){
-        opcion=Integer.parseInt(JOptionPane.showInputDialog("MENÚ:\n"+
-                "1 añadir correo\n"+
-                "2 eliminar correo\n"+
-                "3 mostrar correo\n"+
-                "4 primer correo no leído\n"+
-                "5 número total de correos\n"+
-                "6 correos por leer\n"+
-                "7 salir"));
-        switch (opcion){
-            case 1:
-                buzon1.añadir(new Correo(false,JOptionPane.showInputDialog("Introduzca argumento")));
-                break;
-            case 2:
-                buzon1.elimina(Integer.parseInt(JOptionPane.showInputDialog("Inserte correo a borrar."))-1);
-                break;
-            case 3:
-                JOptionPane.showMessageDialog(null, buzon1.mostrar(Integer.parseInt(JOptionPane.showInputDialog("Inserte correo a mostrar"))-1));
-                break;
-            case 4:
-                JOptionPane.showMessageDialog(null,"primer correo no leído: "+buzon1.mostrarPrimerNoLeido());
-                break;
-            case 5:
-                JOptionPane.showMessageDialog(null,"número total de correos: "+buzon1.numeroDeCorreos());
-                break;
-            case 6:
-                JOptionPane.showMessageDialog(null,"correos por leer: "+buzon1.porLeer());
-                break;
-            case 7:
-                encendido=false;
-                break;
-            default:
-                break;
+
+        while(encendido==true){
+            opcion=Integer.parseInt(JOptionPane.showInputDialog("MENÚ:\n"
+                    +"1 añadir correo\n"
+                    +"2 eliminar correo\n"
+                    +"3 mostrar correo\n"
+                    +"4 primer correo no leído\n"
+                    +"5 número total de correos\n"
+                    +"6 correos por leer\n"
+                    +"7 salir"));
+            switch(opcion){
+                case 1:
+                    buzon1.añadir(new Correo(false, JOptionPane.showInputDialog("Introduzca argumento")));
+                    break;
+                case 2:
+                    buzon1.elimina(Integer.parseInt(JOptionPane.showInputDialog("Inserte correo a borrar."))-1);
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, buzon1.mostrar(Integer.parseInt(JOptionPane.showInputDialog("Inserte correo a mostrar"))-1));
+                    break;
+                case 4:
+                    JOptionPane.showMessageDialog(null, "primer correo no leído: "+buzon1.mostrarPrimerNoLeido());
+                    break;
+                case 5:
+                    JOptionPane.showMessageDialog(null, "número total de correos: "+buzon1.numeroDeCorreos());
+                    break;
+                case 6:
+                    JOptionPane.showMessageDialog(null, "correos por leer: "+buzon1.porLeer());
+                    break;
+                case 7:
+                    encendido=false;
+                    System.exit(0);
+                default:
+                    break;
+            }
         }
     }
-    }
-    
+
 }
